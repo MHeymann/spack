7d09664a35bbf32c8e45be621df5ed4f08cd280d
Murray Heymann <heymann.murray@gmail.com>
2022-04-01 15:20:24 +0200

    Update versions available for flint, refactor
    Also update the url to use flint's own library. Create proper install
    phases, including an optional test phase.
    
    Add maintaners, including the maintener of flint's own github page.

--- var/spack/repos/builtin/packages/flint/package.py
+++ var/spack/repos/builtin/packages/flint/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -10,39 +10,44 @@ class Flint(Package):
     """FLINT (Fast Library for Number Theory)."""
 
     homepage = "https://www.flintlib.org"
-    url      = "https://mirrors.mit.edu/sage/spkg/upstream/flint/flint-2.5.2.tar.gz"
+    url      = "https://www.flintlib.org/flint-2.8.4.tar.gz"
     git      = "https://github.com/wbhart/flint2.git"
 
-    version('develop', branch='trunk')
-    version('2.5.2', sha256='cbf1fe0034533c53c5c41761017065f85207a1b770483e98b2392315f6575e87')
-    version('2.4.5', sha256='e489354df00f0d84976ccdd0477028693977c87ccd14f3924a89f848bb0e01e3')
+    maintainers = ["wbhart", "MHeymann"]
 
-    # Overlap in functionality between gmp and mpir
-    # All other dependencies must also be built with
-    # one or the other
-    # variant('mpir', default=False,
-    #         description='Compile with the MPIR library')
+
+    version('develop', branch='trunk')
+    version('2.8.4', sha256='61df92ea8c8e9dc692d46c71d7f50aaa09a33d4ba08d02a1784730a445e5e4be')
+    version('2.7.1', sha256='186e2fd9ab67df8a05b122fb018269b382e4babcdb17353c4be1fe364dca481e')
+    version('2.6.3', sha256='ce1a750a01fa53715cad934856d4b2ed76f1d1520bac0527ace7d5b53e342ee3')
 
     # Build dependencies
     depends_on('autoconf', type='build')
 
     # Other dependencies
-    depends_on('gmp')   # mpir is a drop-in replacement for this
-    depends_on('mpfr')  # Could also be built against mpir
+    depends_on('gmp')
+    depends_on('mpfr')
+
+    phases = ['configure', 'build', 'install']
+
+    def configure_args(self):
+        args = [ "--prefix=%s" % self.prefix
+               , "--with-gmp=%s" % self.spec['gmp'].prefix
+               , "--with-mpfr=%s" % self.spec['mpfr'].prefix
+               ]
+        return args
+
+    def configure(self, spec, prefix):
+        configure_script = Executable("./configure")
+        configure_script(*self.configure_args())
+
+    def build(self, spec, prefix):
+        make()
+
+    @run_after('build')
+    @on_package_attributes(run_tests=True)
+    def test(self):
+        make("check")
 
     def install(self, spec, prefix):
-        options = []
-        options = ["--prefix=%s" % prefix,
-                   "--with-gmp=%s" % spec['gmp'].prefix,
-                   "--with-mpfr=%s" % spec['mpfr'].prefix]
-
-        # if '+mpir' in spec:
-        #     options.extend([
-        #         "--with-mpir=%s" % spec['mpir'].prefix
-        #     ])
-
-        configure(*options)
-        make()
-        if self.run_tests:
-            make("check")
         make("install")
